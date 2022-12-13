9e855e8e836379b36452436975898f8eb991bac3
Amit Elrom <amitelrom99@gmail.com>
2022-12-13 12:46:19 +0200

    add: to db

--- index.js
+++ index.js
@@ -55,7 +55,224 @@ const keys = [
       hebrew: "ע",
     },
   },
+  {
+    key: ["H", "h", "י"],
+    languages: {
+      englishCpitalized: "H",
+      english: "h",
+      hebrew: "י",
+    },
+  },
+  {
+    key: ["I", "i", "ן"],
+    languages: {
+      englishCpitalized: "I",
+      english: "i",
+      hebrew: "ן",
+    },
+  },
+  {
+    key: ["J", "j", "ח"],
+    languages: {
+      englishCpitalized: "J",
+      english: "j",
+      hebrew: "ח",
+    },
+  },
+  {
+    key: ["K", "k", "ל"],
+    languages: {
+      englishCpitalized: "K",
+      english: "k",
+      hebrew: "ל",
+    },
+  },
+  {
+    key: ["L", "l", "ך"],
+    languages: {
+      englishCpitalized: "L",
+      english: "l",
+      hebrew: "ך",
+    },
+  },
+  {
+    key: ["M", "m", "צ"],
+    languages: {
+      englishCpitalized: "M",
+      english: "m",
+      hebrew: "צ",
+    },
+  },
+  {
+    key: ["N", "n", "מ"],
+    languages: {
+      englishCpitalized: "N",
+      english: "n",
+      hebrew: "מ",
+    },
+  },
+  {
+    key: ["O", "o", "ם"],
+    languages: {
+      englishCpitalized: "O",
+      english: "o",
+      hebrew: "ם",
+    },
+  },
+  {
+    key: ["P", "p", "פ"],
+    languages: {
+      englishCpitalized: "P",
+      english: "p",
+      hebrew: "פ",
+    },
+  },
+  {
+    key: ["Q", "q", "/"],
+    languages: {
+      englishCpitalized: "Q",
+      english: "q",
+      hebrew: "/",
+    },
+  },
+  {
+    key: ["R", "r", "ר"],
+    languages: {
+      englishCpitalized: "R",
+      english: "r",
+      hebrew: "ר",
+    },
+  },
+  {
+    key: ["S", "s", "ד"],
+    languages: {
+      englishCpitalized: "S",
+      english: "s",
+      hebrew: "ד",
+    },
+  },
+  {
+    key: ["T", "t", "א"],
+    languages: {
+      englishCpitalized: "T",
+      english: "t",
+      hebrew: "א",
+    },
+  },
+  {
+    key: ["U", "u", "ו"],
+    languages: {
+      englishCpitalized: "U",
+      english: "u",
+      hebrew: "ו",
+    },
+  },
+  {
+    key: ["V", "v", "ה"],
+    languages: {
+      englishCpitalized: "V",
+      english: "v",
+      hebrew: "ה",
+    },
+  },
+  {
+    key: ["W", "w", "'"],
+    languages: {
+      englishCpitalized: "W",
+      english: "w",
+      hebrew: "'",
+    },
+  },
+  {
+    key: ["X", "x", "ס"],
+    languages: {
+      englishCpitalized: "X",
+      english: "x",
+      hebrew: "ס",
+    },
+  },
+  {
+    key: ["Y", "y", "ט"],
+    languages: {
+      englishCpitalized: "Y",
+      english: "y",
+      hebrew: "ט",
+    },
+  },
+  {
+    key: ["Z", "z", "ז"],
+    languages: {
+      englishCpitalized: "Z",
+      english: "z",
+      hebrew: "ז",
+    },
+  },
+  {
+    key: ["Z", "z", "ז"],
+    languages: {
+      englishCpitalized: "Z",
+      english: "z",
+      hebrew: "ז",
+    },
+  },
+  {
+    key: [";", ";", "ף"],
+    languages: {
+      englishCpitalized: ";",
+      english: ";",
+      hebrew: "ף",
+    },
+  },
+  {
+    key: [";", ";", "ף"],
+    languages: {
+      englishCpitalized: ";",
+      english: ";",
+      hebrew: "ף",
+    },
+  },
+  {
+    key: [",", ",", "ת"],
+    languages: {
+      englishCpitalized: ",",
+      english: ",",
+      hebrew: "ת",
+    },
+  },
+  {
+    key: [".", ".", "ץ"],
+    languages: {
+      englishCpitalized: ".",
+      english: ".",
+      hebrew: "ץ",
+    },
+  },
+  {
+    key: ["/", "/", "."],
+    languages: {
+      englishCpitalized: "/",
+      english: "/",
+      hebrew: ".",
+    },
+  },
+  {
+    key: ["'", "'", ","],
+    languages: {
+      englishCpitalized: "'",
+      english: "'",
+      hebrew: ",",
+    },
+  },
+  {
+    key: ["'", "'", ","],
+    languages: {
+      englishCpitalized: "'",
+      english: "'",
+      hebrew: ",",
+    },
+  },
 ];
+
 
 let lng = "";
 const selectLanguage = () => {
